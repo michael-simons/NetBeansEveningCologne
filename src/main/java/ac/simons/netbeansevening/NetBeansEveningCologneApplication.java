d446774e5ca0129fba485422cf9c5696ea97f197
Michael J. Simons <michael@simons.ac>
2016-08-24 12:18:01 +0200

    Use NB-SpringBoot to access start.spring.io and generate a new project
    Give the project a groupId and an artifactId as well as name and description. Furthermore, chose the first dependencies:
    
    * web
    * jpa
    * devtools
    * and h2 as a runtime dependency
    
    By having h2 on the classpath, Spring Boot automatically configures an inmemory datasource for use during development and testing with JPA.
    
    The devtools bring a reload server for web content as well as a modified class loader that supports changed class files. To enable a smooth debugging experience while using devetools, turn off "Apply code changes after save" for the Java debugger in NetBeans.

---
+++ src/main/java/ac/simons/netbeansevening/NetBeansEveningCologneApplication.java
@@ -0,0 +1,12 @@
+package ac.simons.netbeansevening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBeansEveningCologneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetBeansEveningCologneApplication.class, args);
+	}
+}
---
+++ src/test/java/ac/simons/netbeansevening/NetBeansEveningCologneApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.simons.netbeansevening;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetBeansEveningCologneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
