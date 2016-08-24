e52142d815b4a33c3650e3c8f4936e77407a1361
Michael J. Simons <michael@simons.ac>
2016-08-24 14:45:56 +0200

    Optimize RegistrationEntity with Lombok

--- src/main/java/ac/simons/netbeansevening/RegistrationEntity.java
+++ src/main/java/ac/simons/netbeansevening/RegistrationEntity.java
@@ -1,7 +1,6 @@
 package ac.simons.netbeansevening;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,11 +8,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+import static lombok.AccessLevel.PROTECTED;
+
 @Entity
 @Table(name = "registrations")
+@Getter
+@EqualsAndHashCode(of = "email")
+@NoArgsConstructor(access = PROTECTED)
 public class RegistrationEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -31,51 +39,11 @@ public class RegistrationEntity implements Serializable {
     @Column(length = 512, nullable = false)
     @NotBlank
     @Size(max = 512)
+    @Setter
     private String name;
-
-    protected RegistrationEntity() {
-    }
 
     public RegistrationEntity(String email, String name) {
 	this.email = email;
 	this.name = name;
-    }    
-    
-    public Integer getId() {
-	return id;
-    }
-
-    public String getEmail() {
-	return email;
-    }
-  
-    public String getName() {
-	return name;
-    }
-
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    @Override
-    public int hashCode() {
-	int hash = 7;
-	hash = 53 * hash + Objects.hashCode(this.email);
-	return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj) {
-	    return true;
-	}
-	if (obj == null) {
-	    return false;
-	}
-	if (getClass() != obj.getClass()) {
-	    return false;
-	}
-	final RegistrationEntity other = (RegistrationEntity) obj;
-	return Objects.equals(this.email, other.email);
-    }    
+    }              
 }
