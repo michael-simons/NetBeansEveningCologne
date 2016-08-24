d40a48a7f15d07dbd0b01a639708469fc938ceab
Michael J. Simons <michael@simons.ac>
2016-08-24 13:08:31 +0200

    Add a CRUD repository for registrations

---
+++ src/main/java/ac/simons/netbeansevening/RegistrationRepository.java
@@ -0,0 +1,6 @@
+package ac.simons.netbeansevening;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegistrationRepository extends CrudRepository<RegistrationEntity, Integer> {
+}
