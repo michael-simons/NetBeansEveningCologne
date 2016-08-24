3aa841d6f0923bf4bc2eae569bb6f9bfb6ea2613
Michael J. Simons <michael@simons.ac>
2016-08-24 15:26:34 +0200

    Fix create

--- src/main/java/ac/simons/netbeansevening/RegistrationController.java
+++ src/main/java/ac/simons/netbeansevening/RegistrationController.java
@@ -2,10 +2,12 @@ package ac.simons.netbeansevening;
 
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -29,6 +31,7 @@ public class RegistrationController {
     }
     
     @RequestMapping(method = POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public RegistrationEntity create(@Valid @RequestBody RegistrationEntity newRegistration) {
 	return this.registrationRepository.save(newRegistration);
     }
